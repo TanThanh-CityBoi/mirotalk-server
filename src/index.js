9f59e2f99646a868b0e965dca084fc869728c40f
tanthanh-cityboi <tanthanhe@gmail.com>
2022-12-21 10:54:30 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const cors = require("cors");
 const express = require("express");
-const route = require("./src/routers/index");
+const route = require("./routers/index");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -13,10 +13,10 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors());
 
-// pass app into router
 route(app);
-const a = '111'
-//app listen
+
+const jdhfjdhfj =  '222'
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
